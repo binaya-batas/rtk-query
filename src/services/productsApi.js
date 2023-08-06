96e5844cea60fa959b0838273525aa40f3699dea
binaya-batas <batas.binaya@gmail.com>
2023-08-06 18:58:02 +0545

    Add get product category, specific categories and add new product.

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -1,14 +1,33 @@
 import { emptyApi } from "./api";
 
 export const productsApi = emptyApi.injectEndpoints({
-  endpoints: (builder) => ({
-    getAllProducts: builder.query({
-      query: () => `products`,
+    endpoints: (builder) => ({
+        getAllProducts: builder.query({
+            query: () => `products`,
+        }),
+        getSingleProduct: builder.query({
+            query: (id) => `products/${id}`,
+        }),
+        getProductCategories: builder.query({
+            query: () => `products/categories`,
+        }),
+        getSpecificCategory: builder.query({
+            query: (categoryName) => `products/${categoryName}`
+        }),
+        addNewProduct: builder.mutation({
+            query: (body) => ({
+                url: `products`,
+                method: 'POST',
+                body
+            })
+        })
     }),
-    getSingleProduct: builder.query({
-      query: (id) => `products/${id}`,
-    }),
-  }),
 });
 
-export const { useGetAllProductsQuery, useGetSingleProductQuery } = productsApi;
+export const { 
+    useGetAllProductsQuery, 
+    useGetSingleProductQuery, 
+    useGetProductCategoriesQuery,
+    useGetSpecificCategoryQuery,
+    useAddNewProductMutation 
+} = productsApi;
